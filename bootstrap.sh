7df8d00849ba96fd36eb857e513798214a275ccc
Eyal Firstenberg <eyal.firstenberg@lightcyber.com>
2013-12-09 01:57:42 +0200

    fixed bug in bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+git submodule update --init --recursive
 
 ln -s ~/personal_config/.vimrc.after ~/.vimrc.after
 ln -s ~/personal_config/.vimrc.before ~/.vimrc.before
@@ -7,4 +8,3 @@ ln -s ~/personal_config/.vim ~/.vim
 ln -s ~/personal_config/.gitconfig ~/.gitconfig
 ln -s ~/personal_config/.bashrc ~/.bashrc
 ln -s ~/personal_config/.byobu/ ~/.byobu
-
