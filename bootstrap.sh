c9759297cd54a07d5da486e92846693ba1120f23
Eyal Firstenberg <eyal.firstenberg@lightcyber.com>
2013-12-09 10:10:49 +0200

    upgraded bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,8 @@
 #! /bin/bash
 git submodule update --init --recursive
 
-ln -s ~/personal_config/.vimrc.after ~/.vimrc.after
-ln -s ~/personal_config/.vimrc.before ~/.vimrc.before
-ln -s ~/personal_config/.janus ~/.janus
-ln -s ~/personal_config/.vim ~/.vim
-ln -s ~/personal_config/.gitconfig ~/.gitconfig
-ln -s ~/personal_config/.bashrc ~/.bashrc
-ln -s ~/personal_config/.byobu/ ~/.byobu
+for file in {.vimrc.after,.vimrc.before,.janus,.vim,.gitconfig,.bashrc,.byobu}
+do
+  mv ~/${file} ~/${file}_backup
+  ln -s ~/personal_config/${file} ~/${file}
+done
