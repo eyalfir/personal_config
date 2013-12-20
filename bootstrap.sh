69a91965da1460b0f6ff27deb2db3c0e34535f07
Eyal Firstenberg <eyal.firstenberg@lightcyber.com>
2013-12-20 23:20:10 +0200

    download latest tmux in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
+
+# download latest tmux
+wget http://downloads.sourceforge.net/project/tmux/tmux/tmux-1.8/tmux-1.8.tar.gz
+tar xvf tmux*gz
+# install tmux dependencies
+sudo apt-get install libevent-dev libncurses-dev pkg-config
+
 git submodule update --init --recursive
 
 for file in {.vimrc.after,.vimrc.before,.janus,.vim,.gitconfig,.bashrc,.byobu,.ipython/profile_default}
