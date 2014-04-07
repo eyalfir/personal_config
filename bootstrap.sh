242967dd74cb5eaf5322808149ceca1146bb8dd9
Eyal Firstenberg <eyal.firstenberg@lightcyber.com>
2014-04-07 07:55:37 -0700

    added status bar to screenrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,11 +3,12 @@
 
 git submodule update --init --recursive
 
-for file in {.vimrc.after,.vimrc.before,.janus,.vim,.gitconfig,.bashrc,.byobu/.tmux.conf,.ipython/profile_default}
+for file in {.vimrc.after,.vimrc.before,.janus,.vim,.gitconfig,.bashrc,.byobu/.tmux.conf,.ipython/profile_default,.screenrc}
 do
   mv ~/${file} ~/${file}_backup
   ln -s ~/personal_config/${file} ~/${file}
 done
+mv ~/.vimrc ~/.vimrc
 ln -s ~/personal_config/.vim/janus/vim/vimrc ~/.vimrc
 
 # download latest tmux
