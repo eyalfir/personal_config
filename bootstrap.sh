b825a28f5c3c69bc3b7c0b2c788bb7a0a41ad21e
Eyal Firstenberg <eyal.firstenberg@lightcyber.com>
2014-05-03 11:24:08 +0300

    seperated bootstrap from link_dotfiels

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,8 @@
 #! /bin/bash
 
+link_dotfiles.sh
 
 git submodule update --init --recursive
-
-for file in {.vimrc.after,.vimrc.before,.janus,.vim,.gitconfig,.bashrc,.byobu/.tmux.conf,.ipython/profile_default,.screenrc}
-do
-  mv ~/${file} ~/${file}_backup
-  ln -s ~/personal_config/${file} ~/${file}
-done
-mv ~/.vimrc ~/.vimrc
-ln -s ~/personal_config/.vim/janus/vim/vimrc ~/.vimrc
-
 # download latest tmux
 wget http://downloads.sourceforge.net/project/tmux/tmux/tmux-1.8/tmux-1.8.tar.gz
 tar xvf tmux*gz
---
+++ link_dotfiles.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+for file in {.vimrc.after,.vimrc.before,.janus,.vim,.gitconfig,.bashrc,.byobu/.tmux.conf,.ipython/profile_default,.screenrc}
+do
+  mv ~/${file} ~/${file}_backup
+  ln -s ~/personal_config/${file} ~/${file}
+done
+mv ~/.vimrc ~/.vimrc_backup
+ln -s ~/personal_config/.vim/janus/vim/vimrc ~/.vimrc
+
