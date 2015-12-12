61331439a749cfbd44144e9d69993e6457983f39
Eyal Firstenberg <eyal.firstenberg@lightcyber.com>
2015-12-12 15:50:42 +0200

    more remove janus

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -1,10 +1,7 @@
 #! /bin/bash
 
-for file in {.ctags,.vimrc.after,.vimrc.before,.janus,.vim,.gitconfig,.bashrc,.byobu/.tmux.conf,.ipython/profile_default/startup,.screenrc}
+for file in {.ctags,.vimrc.after,.vimrc.before,..gitconfig,.bashrc,.byobu/.tmux.conf,.ipython/profile_default/startup,.screenrc}
 do
   mv ~/${file} ~/${file}_backup
   ln -s ~/personal_config/${file} ~/${file}
 done
-mv ~/.vimrc ~/.vimrc_backup
-ln -s ~/personal_config/.vim/janus/vim/vimrc ~/.vimrc
-
