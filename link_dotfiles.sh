f9a3eef35092caf85e972d192312f5b4474cb527
Eyal Firstenberg <eyal.firstenberg@lightcyber.com>
2017-06-04 10:16:47 +0300

    improved link_dotfiles.sh to allow for directories

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
-for file in {.tmux,.ctags,.vimrc.after,.vimrc.before,.gitconfig,.bashrc,.byobu/.tmux.conf,.ipython/profile_default/startup,.screenrc}
+for file in {.tmux,.ctags,.vimrc.after,.vimrc.before,.gitconfig,.bashrc,.tmux.conf,.ipython/profile_default/startup,.screenrc,.config/powerline}
 do
+  mkdir -p ~/$(dirname $file)
   mv ~/${file} ~/${file}_backup
   ln -s ~/personal_config/${file} ~/${file}
 done
